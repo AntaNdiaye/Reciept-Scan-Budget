611f2768c3748ec5eb975aace5b7a820740407d0
Yamilet <52222018+gutierya@users.noreply.github.com>
2021-07-13 10:37:29 -0400

    corrected enum class

--- app/backend/src/Models/Notification.java
+++ app/backend/src/Models/Notification.java
@@ -7,42 +7,5 @@ public enum Notification {
 	private int REPEAT_PURCHASE;
 	private int WITHIN_BUDGET;
 
-	Notification(int OVER_BUDGET, int REPEAT_PURCHASE, int WITHIN_BUDGET) {
-		this.OVER_BUDGET = OVER_BUDGET;
-		this.REPEAT_PURCHASE = REPEAT_PURCHASE;
-		this.WITHIN_BUDGET = WITHIN_BUDGET;
-	}
 
-	public int getOVER_BUDGET() {
-		return OVER_BUDGET;
-	}
-
-	public void setOVER_BUDGET(int OVER_BUDGET) {
-		this.OVER_BUDGET = OVER_BUDGET;
-	}
-
-	public int getREPEAT_PURCHASE() {
-		return REPEAT_PURCHASE;
-	}
-
-	public void setREPEAT_PURCHASE(int REPEAT_PURCHASE) {
-		this.REPEAT_PURCHASE = REPEAT_PURCHASE;
-	}
-
-	public int getWITHIN_BUDGET() {
-		return WITHIN_BUDGET;
-	}
-
-	public void setWITHIN_BUDGET(int WITHIN_BUDGET) {
-		this.WITHIN_BUDGET = WITHIN_BUDGET;
-	}
-
-	@Override
-	public String toString() {
-		return "Notification{" +
-				"OVER_BUDGET=" + OVER_BUDGET +
-				", REPEAT_PURCHASE=" + REPEAT_PURCHASE +
-				", WITHIN_BUDGET=" + WITHIN_BUDGET +
-				'}';
-	}
 }
