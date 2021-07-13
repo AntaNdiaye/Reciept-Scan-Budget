3599f2e05ac90cd1e0136cb5a32b5f182bfc6323
jasminelin18 <71404828+jasminelin18@users.noreply.github.com>
2021-07-13 10:35:44 -0400

    added repo

--- app/backend/src/Repository/recieptRepo.java
+++ app/backend/src/Repository/recieptRepo.java
@@ -1,4 +1,9 @@
 package Repository;
 
-public class recieptRepo {
+import Models.Receipt;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface recieptRepo extends JpaRepository<Receipt, Long> {
 }
