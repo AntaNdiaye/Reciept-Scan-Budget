7ec3b4f9cc424aeb59a738125074b4891d2dec6e
jasminelin18 <71404828+jasminelin18@users.noreply.github.com>
2021-07-13 14:13:31 -0400

    fixed enums

---
+++ app/backend/src/Models/Enums.java
@@ -0,0 +1,12 @@
+package Models;
+
+public interface Enums {
+
+    public enum Categories {
+        FOOD, CLOTHING, EDUCATION, TRAVEL, ENTERTAINMENT, MEDICAL, HOME
+    }
+
+    public enum Notification {
+        OVER_BUDGET, REPEAT_PURCHASE, WITHIN_BUDGET,
+    }
+}
